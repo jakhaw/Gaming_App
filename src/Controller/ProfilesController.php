308048e131948815130e690281d0a9b212953d21
Jakub Hawryłkowicz <hawrylkowicz.jakub@gmail.com>
2023-06-14 18:04:55 +0200

    Setting up the project

---
+++ src/Controller/ProfilesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfilesController extends AbstractController
+{
+    #[Route('/profiles', name: 'app_profiles')]
+    public function index(): Response
+    {
+        return $this->render('profiles/index.html.twig');
+    }
+}
