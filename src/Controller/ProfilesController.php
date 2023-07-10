30a21b5d2404a0a38bcf25bb6fbd90099a781310
Jakub Hawryłkowicz <hawrylkowicz.jakub@gmail.com>
2023-07-10 14:22:19 +0200

    Adding user restrictions to links

--- src/Controller/ProfilesController.php
+++ src/Controller/ProfilesController.php
@@ -2,25 +2,15 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProfilesController extends AbstractController
 {
-    private $userRepository;
-
-    public function __construct(UserRepository $userRepository)
+    #[Route('/profile/', methods:["GET"], name: 'app_profiles')]
+    public function index(): Response
     {
-        $this->userRepository = $userRepository;
-    }
-
-    #[Route('/profile/{id}', methods:["GET"], name: 'app_profiles')]
-    public function index($id): Response
-    {
-        return $this->render('profiles/index.html.twig', [
-            'userInfo' => $this->userRepository->find($id),
-        ]);
+        return $this->render('profiles/index.html.twig');
     }
 }
